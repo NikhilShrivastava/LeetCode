fd1c26ec2f474f320db6d2a36d4542de34aa0b93
Nikhil Shrivastava <nikhilshrivastava175@gmail.com>
2020-06-02 01:44:57 +0530

    Add files via upload

---
+++ June Challenge/Day-1.cpp
@@ -0,0 +1,15 @@
+TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL)
+            return NULL;
+        else
+        {
+            struct TreeNode *temp;
+            invertTree(root->left);
+            invertTree(root->right);
+            temp=root->left;
+            root->left=root->right;
+            root->right=temp;
+            return root;
+        }
+        
+    }
