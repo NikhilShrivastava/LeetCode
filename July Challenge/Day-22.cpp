f938b059f3415146af2f25ec0277c7ebcbbce40a
Nikhil Shrivastava <nikhilshrivastava175@gmail.com>
2020-07-24 01:34:31 +0530

    Binary Tree Zigzag Level Order Traversa

---
+++ July Challenge/Day-22.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        int n = depth(root);
+        vector<vector<int>> res(n, vector<int>());
+        traverse(root, res, 0);
+        for (int i = 1; i < n; i += 2) {
+            reverse(res[i].begin(), res[i].end());
+        }
+        return res;
+    }
+    int depth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        return max(depth(root->left), depth(root->right)) + 1;
+    }
+    void traverse(TreeNode* root, vector<vector<int>>& res, int level) {
+        if (root == nullptr) return;
+        res[level].push_back(root->val);
+        traverse(root->left, res, level+1);
+        traverse(root->right, res, level+1);
+    }
+};
